9ff0b85842d8a2b577686785c4d3026d0e18fb5c
chinna <chinnajanapareddyy@gmail.com>
2026-09-10 22:11:28 +0530

    Fix Vite CSS bundle import for Vercel deployment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import {
 import { useEffect, useState, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
+import "../styles.css";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { BowLogo } from "../components/bow-ui";
 import { WelcomeScreen } from "../components/bow-welcome";
